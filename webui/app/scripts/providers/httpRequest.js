bfefebe4e5108d898d2a934257839bf8b69e18c8
tianyingchun <tianyingchun@outlook.com>
2014-10-31 21:09:06 +0800

    updates'

--- webui/app/scripts/providers/httpRequest.js
+++ webui/app/scripts/providers/httpRequest.js
@@ -1,4 +1,4 @@
-app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remoteApi',
+app.factory("httpRequest", ['$log', '$http', '$q', 'utility', 'appConfig', 'remoteApi',
 
     function($log, $http, $q, utility, appConfig, remoteApi) {
 
@@ -9,6 +9,11 @@ app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remo
         var commonHeader = {
             "Authorization": "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="
         };
+        // default configurations. no need to use common header now.
+        // angular.extend($http.defaults.headers.common, commonHeader);
+        // $http.defaults.headers.post
+        // $http.defaults.headers.get
+
         // get current api async request data object.
         function getRequestData(data) {
             // default request data
@@ -24,33 +29,44 @@ app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remo
             return remoteApi.apiBaseUrl + apiUrl;
         };
 
+        // for get request get params parameter
+        function getSerializedUrl(url, requestData) {
+            var oParam = {};
+            requestData = angular.isArray(requestData) ? requestData : [requestData];
+            for (var i = 0; i < requestData.length; i++) {
+                var oCur = requestData[i];
+                for (var p in oCur) {
+                    oParam[p] = encodeURIComponent(oCur[p]);
+                }
+            }
+
+            var newUrlParamStr = "";
+            for (var i in oParam) {
+                newUrlParamStr += i + "=" + oParam[i] + "&";
+            }
+            return url.split("?")[0] + "?" + newUrlParamStr.substring(0, newUrlParamStr.length - 1);
+        };
         // Expose base http request constructor.
         return function BaseHttpRequest() {
 
             /**
-             * http request.
-             * @param  {string} url     request url
-             * @param  {string} method  GET,POST default is POST.
-             * @param  {data} data      the parameters for GET, POST
-             * @param  {function} dto   optional,customized data converter.
-             * @param  {object/string/array} customizedData   optional,allow us passed customized data into DTO converter callback.
-            
-             * @param  {object} headers {post|get|put:{ "Content-Type":"application/json"}}
+             * the post request
+             * @param  {string} url            [description]
+             * @param  {object} requestData    {name:'sss',password:''}
+             * @param  {function} dto            optonal
+             * @param  {object} customizedData passed to dto
+             * @param  {object} config    configJson {headers:{},xxxx}
              */
-            this.postRequest = function(url, requestData, dto, customizedData, postHeaders) {
+            this.postRequest = function(url, requestData, dto, customizedData, config) {
                 // must be isntance it in each request.
                 var defered = $q.defer();
-                // default configurations. no need to use common header now.
-                // $http.defaults.headers.common = commonHeader;
-                // attached other header configurations here.
-                angular.extend($http.defaults.headers.post, postHeaders);
 
                 // request requestData.
                 requestData = getRequestData(requestData) || {};
 
                 var logKey = this.logAPIUniqueKey;
 
-                $http.post(url, requestData).success(function(data, status, headers, config) {
+                $http.post(url, requestData, config).success(function(data, status, headers, config) {
                     // converted raw data.
                     var result = utility.httpRespDataConverter(data, status);
                     // customized dto if available.
@@ -66,29 +82,32 @@ app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remo
                 }).error(function(data, status, headers, config) {
                     var result = utility.httpRespDataConverter(data, status);
                     $log.debug(logKey, utility.stringFormat("failed -> converted data {0}", result));
-
                     return defered.reject(result);
                 });
+
                 return defered.promise;
             };
-
-            this.getRequest = function(url, requestData, dto, customizedData, getHeaders) {
+            /**
+             * the get request
+             * @param  {string} url            [description]
+             * @param  {object} requestData    {name:'sdfsf', password:''}
+             * @param  {function} dto            optonal
+             * @param  {object} customizedData passed to dto
+             * @param  {object} config    configJson {headers:{},xxxx}
+             
+             */
+            this.getRequest = function(url, requestData, dto, customizedData, config) {
 
                 var defered = $q.defer();
-
-                // default configurations. no need to use common header now.
-                // $http.defaults.headers.common = commonHeader;
-                // attached other header configurations here.
-                angular.extend($http.defaults.headers, getHeaders);
-
                 // request requestData.
-                requestData = getRequestData(requestData);
+                requestData = getRequestData(requestData) || {};
+
+                // get serialized Url
+                url = getSerializedUrl(url, requestData);
 
                 var logKey = this.logAPIUniqueKey || "Not Defined Log Key!";
 
-                $http.get(url, {
-                    params: requestData
-                }).success(function(data, status, headers, config) {
+                $http.get(url, config).success(function(data, status, headers, config) {
                     // converted raw data.
                     var result = utility.httpRespDataConverter(data, status);
                     // customized dto if available.
@@ -108,8 +127,9 @@ app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remo
                 });
                 return defered.promise;
             };
+
             // in the most time ,we can invoke remoteRequest to send http request
-            this.remoteRequest = function(url, requestData, dto, customizedData, getHeaders) {
+            this.remoteRequest = function(url, requestData, dto, customizedData, config
                 var promise;
                 var valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
                 if (!valid) {
@@ -117,21 +137,21 @@ app.factory("httpRequest", ['$log', '$http', "$q", 'utility', 'appConfig', 'remo
                 }
                 switch (defaultMethod) {
                     case "POST":
-                        promise = this.postRequest(url, requestData, dto, customizedData, getHeaders);
+                        promise = this.postRequest(url, requestData, dto, customizedData, config
                         break;
                     case "GET":
-                        promise = this.getRequest(url, requestData, dto, customizedData, getHeaders);
+                        promise = this.getRequest(url, requestData, dto, customizedData, config
                         break;
                     default:
-                        promise = this.postRequest(url, requestData, dto, customizedData, getHeaders);
+                        promise = this.postRequest(url, requestData, dto, customizedData, config
                         break;
                 }
                 return promise;
             };
             // used to ajax fetch project assets.
-            this.localRequest = function(url, requestData, dto, customizedData, getHeaders) {
+            this.localRequest = function(url, requestData, dto, customizedData, config
                 var url = appConfig.getTemplateUrl(url);
-                var promise = this.getRequest(url, requestData, dto, customizedData, getHeaders);
+                var promise = this.getRequest(url, requestData, dto, customizedData, config
                 return promise;
             };
         };
