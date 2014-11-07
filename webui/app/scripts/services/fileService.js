8d2dd8a0d94366cf8e0a4fd2955eb78108bafa85
unknown <tanyunlo@TANYUNLO4.asiapacific.hpqcorp.net>
2014-11-08 00:51:37 +0800

    remove lib, use bower to manage third lib. add file serivce to deal with file

---
+++ webui/app/scripts/services/fileService.js
@@ -0,0 +1,42 @@
+(function(app){
+	app.factory('FileService', ['$q', function($q){
+		var FileService = {};
+
+		var fs = require('fs-extra');
+
+		FileService.readFile = function (path) {
+			var defered = $q.defer();
+			fs.readFile(path, 'utf8', function (err, file) {
+				if (err) {
+					return defered.reject(err);
+				}
+				return defered.resolve(file);
+			});
+			return defered.promise;
+		};
+
+		FileService.readJson = function (path) {
+			var defered = $q.defer();
+			fs.readJson(path, function (err, file) {
+				if (err) {
+					return defered.reject(err);
+				}
+				return defered.resolve(file);
+			});
+			return defered.promise;
+		};
+
+		FileService.writeJson = function (path, obj) {
+			var defered = $q.defer();
+			fs.writeJson(path, obj, function (err, file) {
+				if (err) {
+					return defered.reject(err);
+				}
+				return defered.resolve(file);
+			});
+			return defered.promise;
+		};
+
+		return FileService;
+	}])
+})(app);
